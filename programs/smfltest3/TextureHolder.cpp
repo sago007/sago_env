4e4cbfc6ab5b64a5eb06b591b5b75464aa5ddf6d
sago007 <poul@poulsander.com>
2014-05-29 17:16:43 +0200

    A 3rd test program fo SMFL. Loading a texture with a class. The next project may be a charector class

---
+++ programs/smfltest3/TextureHolder.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   TextureHolder.cpp
+ * Author: poul
+ * 
+ * Created on 29. maj 2014, 14:05
+ */
+
+#include "TextureHolder.hpp"
+#include <iostream>
+#include <mutex>
+
+namespace {
+std::mutex mutex_texture_load;
+}  //anonymous namespace
+
+TextureHolder::TextureHolder() {
+}
+
+TextureHolder::~TextureHolder() {
+}
+
+const sf::Texture* TextureHolder::getTexturePtr(const std::string &textureName) const {
+	std::lock_guard<std::mutex> guard(mutex_texture_load);
+	sf::Texture *ret = textures[textureName];
+	if (ret) {
+		return ret;
+	}
+	else {
+		ret = new sf::Texture();
+	}
+	bool success = ret->loadFromFile("data/textures/"+textureName+".png");
+	if (!success) {
+		std::cerr << "getTextureFailed to load " << textureName << std::endl;
+	}
+	return ret;
+}
---
+++ programs/smfltest3/smfltest3.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <boost/program_options.hpp>
+#include "TextureHolder.hpp"
+#include "SagoSprite.hpp"
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+	boost::program_options::options_description desc("Allowed options");
+	desc.add_options()
+		("help,h", "Print basic usage information to stdout and quits")
+	;
+	boost::program_options::variables_map vm;
+	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+	boost::program_options::notify(vm);
+	if (vm.count("help")) {
+		cout << desc << endl;
+		return 1;
+	}
+	TextureHolder texHolder;
+	SagoSprite spUp(texHolder,"golem-walk",sf::IntRect(0,64*0,64,64),7,100);
+	SagoSprite spLeft(texHolder,"golem-walk",sf::IntRect(0,64*1,64,64),7,100);
+	SagoSprite spDown(texHolder,"golem-walk",sf::IntRect(0,64*2,64,64),7,100);
+	SagoSprite spRight(texHolder,"golem-walk",sf::IntRect(0,64*3,64,64),7,100);
+	SagoSprite spRightFast(texHolder,"golem-walk",sf::IntRect(0,64*3,64,64),7,50); //A varaint that walks twise as fast
+	
+	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Test 3");
+	sf::Clock clock;  //start the clock
+	sf::Int32 lastFrameTime = 0;
+	while (window.isOpen()) {
+		sf::Int32 frameTime = clock.getElapsedTime().asMilliseconds();
+		sf::Int32 deltaTime = frameTime - lastFrameTime;
+		float fDeltaTime = static_cast<float>(deltaTime);
+		lastFrameTime = frameTime;
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		window.clear();
+		spUp.Draw(window,frameTime,100,100);
+		spLeft.Draw(window,frameTime,100,200);
+		spRight.Draw(window,frameTime,100,300);
+		spRightFast.Draw(window,frameTime,200,300);
+		spDown.Draw(window,frameTime,100,400);
+		window.display();
+	}
+	return 0;
+}
