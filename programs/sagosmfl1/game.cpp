7d6188d845900bdb936089053c0206b13bf449a6
sago007 <poul@poulsander.com>
2014-11-29 19:25:31 +0100

    game.cpp added to sagosmfl1

---
+++ programs/sagosmfl1/game.cpp
@@ -0,0 +1,11 @@
+/* 
+ * File:   game.cpp
+ * Author: poul
+ * 
+ * Created on 29. november 2014, 14:36
+ */
+
+#include "game.hpp"
+
+game::game() {
+}
---
+++ programs/sagosmfl1/game.hpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   game.hpp
+ * Author: poul
+ *
+ * Created on 29. november 2014, 14:36
+ */
+
+#ifndef GAME_HPP
+#define	GAME_HPP
+
+#include "sago/GameState.hpp"
+#include "sago/SagoMenu.hpp"
+#include "sago/SagoDataHolder.hpp"
+
+class game : sago::GameState {
+public:
+	game();
+private:
+
+};
+
+#endif	/* GAME_HPP */
+
