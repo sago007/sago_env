1426ccedd505e2ca01cb9b973973e107245ece79
sago007 <poul@poulsander.com>
2015-08-19 21:18:12 +0200

    Command to create a basic Java program with Maven

---
+++ java/baseprogram/run_result.sh
@@ -0,0 +1,1 @@
+java -jar target/baseprogram-1.0-SNAPSHOT.jar
---
+++ java/baseprogram/src/main/java/sago/baseprogram/baseprogramMain.java
@@ -0,0 +1,7 @@
+package sago.baseprogram;
+
+public class baseprogramMain {
+    public final static void main(String[] arg) {
+        System.out.println("Basic program!");
+    }
+}
