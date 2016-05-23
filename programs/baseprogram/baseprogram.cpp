f849418f298fb31f807b0b4ac82e732d2bce02a9
sago007 <poul@poulsander.com>
2016-05-23 21:13:29 +0200

    Replaced std::endl by "\n"

--- programs/baseprogram/baseprogram.cpp
+++ programs/baseprogram/baseprogram.cpp
@@ -12,19 +12,19 @@ int main(int argc, const char* const argv[]) {
 	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
 	boost::program_options::notify(vm);
 	if (vm.count("help")) {
-		std::cout << desc << std::endl;
+		std::cout << desc << "\n";
 		return 0;
 	}
 	if (vm.count("version")) {
-		std::cout << "baseprogram 0.1.0" << std::endl;
+		std::cout << "baseprogram 0.1.0\n";
 		return 0;
 	}
 	if (vm.count("somestring")) {
 		std::string somestring = vm["somestring"].as<std::string>();
-		std::cout << "Called with a parameter value of: " << somestring << std::endl;
+		std::cout << "Called with a parameter value of: " << somestring << "\n";
 	}
 	else {
-		std::cout << "This program demonstrates boost::program_options. Try \"" << argv[0] << " --help\" or \"" << argv[0] << " --somestring hello\"" << std::endl;
+		std::cout << "This program demonstrates boost::program_options. Try \"" << argv[0] << " --help\" or \"" << argv[0] << " --somestring hello\"\n";
 	}
 	return 0;
 }
