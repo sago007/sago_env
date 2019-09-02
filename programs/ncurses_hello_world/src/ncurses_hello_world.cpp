a79db1177f5f9b95320b210713c2ac92142fd1cc
sago007 <poul@poulsander.com>
2019-09-02 21:42:51 +0200

    Add Ncurses sample

---
+++ programs/ncurses_hello_world/src/ncurses_hello_world.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <boost/program_options.hpp>
+#include <ncurses.h>
+
+#ifndef VERSIONNUMBER
+#define VERSIONNUMBER "0.1.0"
+#endif
+
+int main(int argc, const char* argv[]) {
+	boost::program_options::options_description desc("Options");
+	desc.add_options()
+	("version", "Print version information and quit")
+	("help,h", "Print basic usage information to stdout and quit")
+	("somestring", boost::program_options::value<std::string>(), "A string to print")
+	;
+	boost::program_options::variables_map vm;
+	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+	boost::program_options::notify(vm);
+	if (vm.count("help")) {
+		std::cout << desc << "\n";
+		return 0;
+	}
+	if (vm.count("version")) {
+		std::cout << "ncurses_hello_world " << VERSIONNUMBER << "\n";
+		return 0;
+	}
+	initscr();
+	if (vm.count("somestring")) {
+		printw(vm["somestring"].as<std::string>().c_str());
+	} else {
+		printw("Hello World !!!");
+	}
+	refresh();
+	getch();
+	endwin();
+	return 0;
+}
