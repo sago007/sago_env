b2c8ff66eb1bec71f98f64a06f11f2d89bd9a527
Poul Sander <poul@unity3d.com>
2022-12-22 19:12:16 +0100

    Handle nested files and keep shortest file (including path)

--- programs/find_duplicates/find_duplicates.py
+++ programs/find_duplicates/find_duplicates.py
@@ -4,22 +4,22 @@ from collections import defaultdict
 import sys
 
 
-
 def process_dir(directory):
     files_by_hash = defaultdict(list)
     for x in os.walk(directory):
         for y in [x[2]]:
             for z in y:
-                openedFile = open(directory+"/"+z,"br")
+                filename = x[0]+"/"+z
+                openedFile = open(filename,"br")
                 readFile = openedFile.read()
                 md5Hash = hashlib.md5(readFile)
                 md5Hashed = md5Hash.hexdigest()
                 openedFile.close()
-                files_by_hash[md5Hashed].append(z)
+                files_by_hash[md5Hashed].append(filename)
     for x in files_by_hash:
-        list_of_files = files_by_hash[x]
+        list_of_files = sorted(files_by_hash[x], key=len)
         for y in list_of_files[1:]:
-            print("rm '{}/{}' # Duplicate of {}".format(directory, y, list_of_files[0]))
+            print("rm '{}' # Duplicate of {}".format(y, list_of_files[0]))
 
 if len(sys.argv) < 2:
     print("No arguments given. Arguments myst be a list of files.")
