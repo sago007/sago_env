fd16fcae8b96659321ff7999f1163a24789c8adb
Poul Sander <poul@poulsander.com>
2024-05-20 18:21:24 +0200

    Fixed the db scripts

--- db/poul_db/deploy_schemas.sh
+++ db/poul_db/deploy_schemas.sh
@@ -1,2 +1,6 @@
-dbsync -i poul_raw.json -s poul_raw
-dbsync -i community_archiver.json -s oa_archive
+sago_quick_sql --sql "CREATE DATABASE IF NOT EXISTS poul_raw" --exec
+export SAGO_CONNECTION_STRING=$(echo $SAGO_CONNECTION_STRING | sed 's/database=[^;]*;/database=poul_raw;/')
+dbsync -i poul_raw.json
+sago_quick_sql --sql "CREATE DATABASE IF NOT EXISTS oa_archive" --exec
+export SAGO_CONNECTION_STRING=$(echo $SAGO_CONNECTION_STRING | sed 's/database=[^;]*;/database=oa_archive;/')
+dbsync -i community_archiver.json
